9f4daff426050fb1146fc010997c1c63bd037163
RamziBach <ramzibach.dev@gmail.com>
2020-12-07 12:28:23 -0500

    Added unfinished Modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header/Header';
 import Calendar from './calendar/Calendar.js';
+import Modal from './modal/Modal';
 import '../style/app.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Header />
       <Calendar />
+      <Modal />
     </>
   );
 };
---
+++ src/components/modal/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modal-box">
+        <div className="modal-header">
+          <h4 className="modal-header-title">April 7 2021</h4>
+          <button className="modal-header-close-btn">
+            <i className="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
